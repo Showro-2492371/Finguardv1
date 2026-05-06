609017565a05b36f36cf722c080d83d7aab5b8d3
Ayush Turak <2492409@cognizant.com>
2026-05-06 12:01:00 +0530

    Kyc Update status added

--- Finguard/FinGuard-main/FinGuard-master-main/FinGuard-master/src/main/java/org/cts/adm/finguard/KycVerification/Repository/KycDocumentRepository.java
+++ Finguard/FinGuard-main/FinGuard-master-main/FinGuard-master/src/main/java/org/cts/adm/finguard/KycVerification/Repository/KycDocumentRepository.java
@@ -2,9 +2,11 @@ package org.cts.adm.finguard.KycVerification.Repository;
 
 import org.cts.adm.finguard.KycVerification.Model.KycDocument;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface KycDocumentRepository extends JpaRepository<KycDocument,Long> {
     Optional<KycDocument> findByCustomerCustomerId(Long customerId);
 
